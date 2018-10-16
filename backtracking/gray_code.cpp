39d3be2f845a8addb9b47d2eee5540aac2446b28
Bjas <zhaslan0526@gmail.com>
2018-10-16 19:39:05 +0600

    add solutions to backtracking

--- backtracking/gray_code.cpp
+++ backtracking/gray_code.cpp
@@ -2,26 +2,21 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
-void getCombination(int n, vector<string> &result, char ch[], int index) {
-  if(n==index) {
-    result.push_back(ch);
+void getCombination(int n, vector<int> &result, int& index) {
+  if(n==0) {
+    result.push_back(index);
     return;
   }
 
-  for(int i=index; i<n; i++) {
-    ch[index] = '0';
-    getCombination(n, result, ch, i+1);
-    ch[index] = '1';
-  }
+  getCombination(n-1, result, index);
+  index = index ^ (1<<(n-1));
+  getCombination(n-1, result, index);
 }
 int main() {
-  int n;
+  int n, num = 0;
   cin>>n;
-  vector<string> result;
-  char ch[n+1];
-  ch[n] = '\0';
-  getCombination(n, result, ch, 0);
-  sort(result.begin(), result.end());
+  vector<int> result;
+  getCombination(n, result, num);
   for(int i=0; i<result.size(); i++)
     cout<<result[i]<<" ";
   return 0;
